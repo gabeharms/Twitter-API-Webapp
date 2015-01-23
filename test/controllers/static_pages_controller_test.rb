0b2313979fca658d629bcb50aa6a6ffb0ff4275f
Gabe Harms <gabeharms@gmail.com>
2015-01-23 17:54:30 +0000

    Added helper that creates page titles

--- test/controllers/static_pages_controller_test.rb
+++ test/controllers/static_pages_controller_test.rb
@@ -3,25 +3,25 @@ require 'test_helper'
 class StaticPagesControllerTest < ActionController::TestCase
   
   def setup
-    @base_title = "| Gabe Harms Website"
+    @base_title = "Gabe Harms Website"
   end
   
   test "should get home" do
     get :home
     assert_response :success
-    assert_select "title", "Home #{@base_title}"
+    assert_select "title", "#{@base_title}"
   end
 
   test "should get help" do
     get :help
     assert_response :success
-     assert_select "title", "Help #{@base_title}"
+     assert_select "title", "Help | #{@base_title}"
   end
   
   test "should get about" do
     get :about
     assert_response :success
-    assert_select "title", "About #{@base_title}"
+    assert_select "title", "About | #{@base_title}"
   end
 
 end
