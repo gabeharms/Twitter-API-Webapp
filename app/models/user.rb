493ff71aaa545836f72f407d68f5e9f85294778a
Gabe Harms <gabeharms@gmail.com>
2015-02-03 00:04:38 +0000

    Added Account Activation

--- app/models/user.rb
+++ app/models/user.rb
@@ -1,10 +1,10 @@
 class User < ActiveRecord::Base
     VALID_EMAIL_REGEX = /\A[\w+\-.]+@[a-z\d\-]+(\.[a-z\d\-]+)*\.[a-z]+\z/i      # This is a regular expression which detail the format that any email must conform to
     
-    attr_accessor :remember_token
-    
-    before_save { self.email = email.downcase }                   # Make sure all saved email addresses are lower case to avoid saving two versions of the same email address
-     
+    attr_accessor :remember_token, :activation_token
+    before_save   :downcase_email
+    before_create :create_activation_digest
+  
     validates :name,  presence: true, length: { maximum: 50 }     # Validates that the name field exists, and is less than 50 chars
     validates :email, presence: true, length: { maximum: 255 },   # Validates that the email field exists, and is less than 255 chars
                     format: { with: VALID_EMAIL_REGEX },
@@ -32,14 +32,39 @@ class User < ActiveRecord::Base
     end
     
     # Returns true if the given token matches the digest.
-    def authenticated?(remember_token)
-        return false if remember_digest.nil?
-        BCrypt::Password.new(remember_digest).is_password?(remember_token)
+    def authenticated?(attribute, token)
+        digest = send("#{attribute}_digest")
+        return false if digest.nil?
+        BCrypt::Password.new(digest).is_password?(token)
     end
     
     # Forgets a user.
     def forget
         update_attribute(:remember_digest, nil)
     end
+    
+    # Activates an account.
+    def activate
+        update_attribute(:activated,    true)
+        update_attribute(:activated_at, Time.zone.now)
+    end
+
+    # Sends activation email.
+    def send_activation_email
+        UserMailer.account_activation(self).deliver_now
+    end
+
+    private
+    
+        # Converts email to all lower-case.
+        def downcase_email
+            self.email = email.downcase
+        end
+
+        # Creates and assigns the activation token and digest.
+        def create_activation_digest
+            self.activation_token  = User.new_token
+            self.activation_digest = User.digest(activation_token)
+        end
 end
 
--- config/routes.rb
+++ config/routes.rb
@@ -16,7 +16,7 @@ Rails.application.routes.draw do
   delete 'logout'  => 'sessions#destroy'
   
   resources :users                                    # Automatically ensures that our Rails application responds to the RESTful URLs
-  
+  resources :account_activations, only: [:edit]  
 
   # The priority is based upon order of creation: first created -> highest priority.
   # See how all your routes lay out with "rake routes".
