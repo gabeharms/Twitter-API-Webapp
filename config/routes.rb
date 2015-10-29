5be62a235cd77c06ee1191c18eb9b29c9ec5cb4a
Gabe Harms <gabeharms@gmail.com>
2015-10-29 21:57:38 +0000

    User Authentication

--- config/routes.rb
+++ config/routes.rb
@@ -1,7 +1,7 @@
 Rails.application.routes.draw do
   root to: 'static_pages#home'
-  get '/authenticate/callback', to: 'sessions#create', as: 'callback'
-  get '/authenticate/failure', to: 'sessions#error', as: 'failure'
+  get '/auth/twitter/callback', to: 'sessions#create', as: 'callback'
+  get '/auth/twitter/failure', to: 'sessions#error', as: 'failure'
   get '/profile', to: 'sessions#show', as: 'profile'
   delete '/signout', to: 'sessions#destroy', as: 'signout'
   
