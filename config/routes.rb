313618423f3346e88404dd2e054c1a29358a9027
Gabe Harms <gabeharms@gmail.com>
2015-10-29 14:00:00 +0100

    Adding Rails App Base

--- config/routes.rb
+++ config/routes.rb
@@ -1,4 +1,31 @@
 Rails.application.routes.draw do
+  get 'password_resets/new'
+
+  get 'password_resets/edit'
+
+  get 'sessions/new'
+
+  get 'users/new'
+
+  root 'static_pages#home'
+  
+  get 'help'    => 'static_pages#help'
+  get 'about'   => 'static_pages#about'
+  get 'contact' => 'static_pages#contact'
+  
+  get 'signup'  => 'users#new'
+  
+  get    'login'   => 'sessions#new'
+  post   'login'   => 'sessions#create'
+  delete 'logout'  => 'sessions#destroy'
+  
+  resources :users                                    # Automatically ensures that our Rails application responds to the RESTful URLs
+  resources :account_activations, only: [:edit] 
+  resources :password_resets,     only: [:new, :create, :edit, :update]
+  resources :microposts,          only: [:create, :destroy]
+  
+  
+
   # The priority is based upon order of creation: first created -> highest priority.
   # See how all your routes lay out with "rake routes".
 
