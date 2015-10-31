4f8871e7ad116867eb4817521666e2e56fbf9f5d
Gabe Harms <gabeharms@gmail.com>
2015-10-31 14:54:50 +0000

    Creating feature to allow you to view lists

--- config/routes.rb
+++ config/routes.rb
@@ -3,8 +3,10 @@ Rails.application.routes.draw do
   get '/auth/twitter/callback', to: 'sessions#create', as: 'callback'
   get '/auth/twitter/failure', to: 'sessions#error', as: 'failure'
   get '/profile', to: 'sessions#show', as: 'profile'
+  post '/profile', to: 'sessions#show', as: 'search'
   delete '/signout', to: 'sessions#destroy', as: 'signout'
   post '/retweet' => 'tweet#retweet'
+  post '/favorite' => 'tweet#favorite'
   post '/reply' => 'tweet#reply'
   post '/lists' => 'tweet#lists'
 
