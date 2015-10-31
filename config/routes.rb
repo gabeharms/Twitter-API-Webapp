2fd08055128023b3f53afea0c82a1b95cb6ed4bc
Gabe Harms <gabeharms@gmail.com>
2015-10-31 11:56:20 +0000

    Adding Jquery Manipulation of Lists Modal

---
+++ config/routes.rb
@@ -0,0 +1,11 @@
+Rails.application.routes.draw do
+  root to: 'static_pages#home'
+  get '/auth/twitter/callback', to: 'sessions#create', as: 'callback'
+  get '/auth/twitter/failure', to: 'sessions#error', as: 'failure'
+  get '/profile', to: 'sessions#show', as: 'profile'
+  delete '/signout', to: 'sessions#destroy', as: 'signout'
+  post '/retweet' => 'tweet#retweet'
+  post '/reply' => 'tweet#reply'
+  post '/lists' => 'tweet#lists'
+
+end
